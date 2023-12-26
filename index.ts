5fa400c396c64808e47e98952541ac86d96ce740
Anas Bouzid <anasbouzid@gmail.com>
2023-12-26 11:39:42 -0600

    move index.ts

---
+++ index.ts
@@ -0,0 +1,6 @@
+import type { LyrixCardProps } from './src/components/LyrixCard';
+import { LyrixCard } from './src/components/LyrixCard';
+import type { LyrixProps } from './src/components/Lyrix';
+import { Lyrix, processLrcLyrics } from './src/components/Lyrix';
+
+export { LyrixCard, LyrixCardProps, Lyrix, LyrixProps, processLrcLyrics };
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import type { LyrixCardProps } from './components/LyrixCard';
-import { LyrixCard } from './components/LyrixCard';
-import type { LyrixProps } from './components/Lyrix';
-import { Lyrix, processLrcLyrics } from './components/Lyrix';
-
-export { LyrixCard, LyrixCardProps, Lyrix, LyrixProps, processLrcLyrics };
