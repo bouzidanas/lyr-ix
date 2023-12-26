e08fd77f6ae2dbb27b42b908506cfbcf951ea2fc
Anas Bouzid <anasbouzid@gmail.com>
2023-12-26 11:36:38 -0600

    update exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ import { LyrixCard } from './components/LyrixCard';
 import type { LyrixProps } from './components/Lyrix';
 import { Lyrix, processLrcLyrics } from './components/Lyrix';
 
-export { LyrixCard, Lyrix, processLrcLyrics };
-export type { LyrixCardProps, LyrixProps };
+export { LyrixCard, LyrixCardProps, Lyrix, LyrixProps, processLrcLyrics };
