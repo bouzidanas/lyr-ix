87cd725b7f4f532b382fa4e6a4b662a73f1bcdc2
bouzidanas <anasbouzid@gmail.com>
2024-10-27 02:19:51 -0500

    update exports

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import type { LyrixCardProps, LyrixCardElement } from './src/components/LyrixCard';
 import { LyrixCard } from './src/components/LyrixCard';
 import type { LyrixProps, ActionsHandle } from './src/components/Lyrix';
-import { Lyrix, processLrcLyrics } from './src/components/Lyrix';
+import { Lyrix } from './src/components/Lyrix';
+import { processLrcLyrics } from './src/util/processLRC';
 
 export { LyrixCard, LyrixCardProps, Lyrix, LyrixProps, processLrcLyrics, LyrixCardElement, ActionsHandle };
