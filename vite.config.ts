feec135ac46781ff74a4d02276664f0e23f9bbf0
Anas Bouzid <anasbouzid@gmail.com>
2024-12-31 15:39:17 -0600

    add Vite lib mode config options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({ jsxImportSource: "@emotion/react" })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, '/index.ts'),
+      name: 'lyr-ix',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
